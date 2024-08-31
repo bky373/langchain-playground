0b117937eedb0d052efc717ecf5bc3570d5d4794
bky373 <bo373@naver.com>
2024-08-31 12:42:01 +0900

    Chaining together components with LCEL

--- build_app_with_lcel.py
+++ build_app_with_lcel.py
@@ -1,7 +1,7 @@
 import os
 
 import dotenv
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
@@ -12,17 +12,17 @@ langchain_api_key = os.getenv("LANGCHAIN_API_KEY")
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 model = ChatOpenAI(model="gpt-4o-mini")
-messages = [
-    SystemMessage(content="Translate the following from English into Korean"),
-    HumanMessage(content="Hi!"),
-]
+# messages = [
+#     SystemMessage(content="Translate the following from English into Korean"),
+#     HumanMessage(content="Hi!"),
+# ]
 
 # result = model.invoke(messages)
 # print(result)
 # AIMessage(content='안녕하세요!' additional_kwargs={'refusal': None} response_metadata={'token_usage': {'completion_tokens': 3, 'prompt_tokens': 20, 'total_tokens': 23}, 'model_name': 'gpt-4o-mini-2024-07-18', 'system_fingerprint': 'fp_f33667828e', 'finish_reason': 'stop', 'logprobs': None} id='run-cf809816-f888-4500-a4fa-097493254214-0' usage_metadata={'input_tokens': 20, 'output_tokens': 3, 'total_tokens': 23})
 
 
-# parser = StrOutputParser()
+parser = StrOutputParser()
 # parsed_result = parser.invoke(result)
 # print(parsed_result)  # 안녕하세요!
 # chain = model | parser
@@ -41,3 +41,8 @@ prompt_template_result = prompt_template.invoke({"language": "Korean", "text": "
 print(prompt_template_result)
 to_messages = prompt_template_result.to_messages()
 print(to_messages)  # [SystemMessage(content='Translate the following into Korean:'), HumanMessage(content='hi')]
+
+chain = prompt_template | model | parser
+chain_result = chain.invoke({"language": "Korean", "text": "hi"})
+print(chain_result)  # 안녕하세요!
+# LangSmith trace: https://smith.langchain.com/public/6b1d2b91-9a46-4202-a8e6-b1edf8917af8/r
